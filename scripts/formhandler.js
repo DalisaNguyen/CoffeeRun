d7077bf1c98ace1fc3b4b5d6322e34a45d8ae25a
Dalisa Nguyen <dalisan@su.fullerton.edu>
2020-09-25 10:09:41 -0700

    initial commit

---
+++ scripts/formhandler.js
@@ -0,0 +1,44 @@
+(function (window) {
+    'use strict';
+    var App = window.App || {};
+    var $ = window.jQuery;
+
+    function FormHandler(selector) {
+        if(!selector) {
+            throw new Error('No selector provided');
+        }
+        this.$formElement = $(selector);
+        if(this.$formElement.length === 0) {
+            throw new Error('Could not find element with selector: ' + selector);        
+        }    
+    }
+    
+    FormHandler.prototype.addSubmitHandler = function(fn) {
+        console.log('Setting submit handler for form');
+        this.$formElement.on('submit', function (event) {
+            event.preventDefault();
+
+            var data = {};
+            $(this).serializeArray().forEach(function(item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value); 
+            });
+            console.log(data);
+            fn(data);
+            this.reset();
+            this.elements[0].focus();
+        });    
+    };
+
+
+    $('#submit-button').click(function(event) {
+        event.preventDefault();
+        this.blur(); // Manually remove focus from clicked link.
+        $.get(this.href, function(html) {
+            $(html).appendTo('body').modal();
+        });
+    });
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+})(window);
